b6893452883f33fbb9a211a99e0724dcbec0e995
Limgyuho <79262519+Limgyuho@users.noreply.github.com>
2023-03-03 21:18:08 +0900

    Add files via upload

---
+++ dist/script.js
@@ -0,0 +1,10 @@
+var divs = $('div');
+
+function 버튼1() {
+    // 구현
+    $('body').css('background-color', 'red');
+}
+function 버튼2() {
+    // 구현
+    $('body').css('background-color', 'blue');
+}
